652c4d3b4cdbd5406c76789c6994cab6269f0441
addie-tyc <addiechung.tyc@gmail.com>
2022-12-16 23:52:12 +0800

    feat: get districts by get method

--- crawl.py
+++ crawl.py
@@ -1,3 +1,8 @@
+import re
+import requests
+
+from bs4 import BeautifulSoup as bs
+
 START, BATCH_SIZE = 0, 1000
 url = 'https://job.taiwanjobs.gov.tw/Internet/Index/job_search_list.aspx'
 ckeys = {'ccga', 'ccgas', '__RequestVerificationToken', 'ASP.NET_SessionId'}
@@ -12,4 +17,15 @@ async def get_cookies(page, dist):
     cookies = await page.cookies()
     cookies = {d['name']: d['value'] for d in cookies if d['name'] in ckeys}
     return cookies
+
+def get_districts():
+    res = requests.get(url)
+    html = bs(res.text, 'html.parser')
+    for num in range(22):
+        city_key = fr'UC_Modal-item_{num}'
+        city = html.find('input', {'id': re.compile(city_key)})
+        dists_key = fr'{city_key}_\d+'
+        dists = html.findAll('input', {'id': re.compile(dists_key)})
+        for dist in dists:
+            yield {'city_name': city['title'], 'name': dist['title'], 'id': dist['id'], 'value': dist['value']}
 
