01e32a1c28f740e701e3a945985936ef1e68a3e6
addie-tyc <addiechung.tyc@gmail.com>
2022-12-17 21:40:24 +0800

    feat: register HrCrawlers in crawl.py

--- crawl.py
+++ crawl.py
@@ -1,5 +1,8 @@
+from HrJobCrawler import HrJobCrawler
 from JobCrawler import JobCrawler
 
 if __name__ == '__main__':
-    job_crawler = JobCrawler()
-    job_crawler.main()
+    JobCrawler().main()
+    crawlers = [ HrJobCrawler(bank) for bank in ['104', '1111', '518', 'cakeresume'] ]
+    for crawler in crawlers:
+        crawler.main()
